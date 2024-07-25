34fcc7860c503dc0a0bd16f3c8b78786fa5f9d33
Sunarto Utina <nartosunarto73126@gmail.com>
2024-07-25 13:25:49 +0700

    UAS Pemrograman Mobile Dasar

---
+++ listView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(listView());
+}
+
+class listView extends StatelessWidget {
+  const listView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
